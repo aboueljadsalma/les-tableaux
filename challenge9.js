573fbb9d5ab4b053a03a38edc8c3acb9691e9269
salma aboueljad <aboueljadsalma@gmail.com>
2026-09-21 15:27:41 +0000

    challege9

--- challenge9.js
+++ challenge9.js
@@ -1,4 +1,17 @@
-let tableau = [5,15,55,155]
-console.log(tableau)
-tableau.reverse();
-console.log(tableau)
+const prompt = require("prompt-sync")();
+let Val = parseInt(prompt("entrez le nombre de l'éléments : "));
+
+const Tab = [];
+
+for (let i = 0 ; i < Val ; i ++) {
+    let Valu = parseInt(prompt("entrez la valure: "))
+    Tab.push(Valu);
+}
+
+const Tab2 = [];
+
+for (let i = Tab.length-1; i >= 0 ; i--) {
+    Tab2.push(Tab[i]);
+}
+
+console.log(Tab2);
