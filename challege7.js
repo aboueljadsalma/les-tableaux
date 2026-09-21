577feef9f327f11ed1753dd03f6e790e5715161c
salma aboueljad <aboueljadsalma@gmail.com>
2026-09-21 11:08:06 +0000

    tableau

--- challege7.js
+++ challege7.js
@@ -3,17 +3,17 @@ const prompt = require('prompt-sync')();
 const nombre = parseInt(prompt("Donnez le nombre des elements : "));
 
 const tableau = [];
-
+let x = 0;
 for (let i = 0; i < nombre; i++) {
     const element = parseInt(prompt("Donnez un nombre : "));
     tableau.push(element);
 }
-for (let i = 0; i < tableau.length; i++) {
+for (let i = 0; i < tableau.length- 1; i++) {
     for (let j = i + 1; j < tableau.length; j++) {
         if (tableau[i] > tableau[j]) {
-            const temp = tableau[i];
-            tableau[i] = tableau[j];
-            tableau[j] = temp;
+            x = tableau [i];
+        tableau[i] = tableau[j];
+            tableau[j] = x;
         }
     }
 }
---
+++ challege8.js
@@ -0,0 +1,4 @@
+let origin = [4,5,8,14];
+let copié = origin.slice(0,4);
+console.log(`origin: ${origin}`);
+console.log(`copié: ${copié}`);
