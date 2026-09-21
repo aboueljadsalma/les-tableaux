2c5eae80c0ffab49ccb6bfdd3351862e05a6f953
salma aboueljad <aboueljadsalma@gmail.com>
2026-09-21 15:11:21 +0000

    challenge8

--- challege8.js
+++ challege8.js
@@ -1,4 +1,18 @@
-let origin = [4,5,8,14];
-let copié = origin.slice(0,4);
+const prompt = require("prompt-sync")();
+
+let u = parseInt(prompt("entrez le nombre de l'éléments: "))
+
+
+const origin = [];
+for (let i = 0 ; i < u ; i++){
+    let v = parseInt(prompt("entrez le nombre de l'élément :"));
+    origin.push(v);
+}
+
+
+const copié = [];
+for (let i = 0 ; i < origin.length ; i++) {
+    copié.push(origin[i])
+}
 console.log(`origin: ${origin}`);
 console.log(`copié: ${copié}`);
